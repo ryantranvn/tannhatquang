4f4ada85e06497bb9ff69a66de79da711f6a89b3
ryantran <ryantran.vn@gmail.com>
2017-09-14 23:45:56 +0700

    tintuc start

--- application/views/frontend/tintuc.php
+++ application/views/frontend/tintuc.php
@@ -6,6 +6,26 @@
                 <?=$this->load->view('frontend/includes/wrap_hot_product','',TRUE)?>
             </div>
             <div id="wrap_right" class="col col-sm-9">
+                <div class="row">
+                    <div class="wrap_tintuc_noibat container-fluid">
+                        <div class="col col-sm-6">
+                            <img src="<?=ASSETS_URL?>frontend/images/tintuc-01.jpg" alt="<?=IMG_ALT?>" />
+                        </div>
+                        <div class="col col-sm-6">
+                            <div class="row">
+                                <img src="<?=ASSETS_URL?>frontend/images/tintuc-02.jpg" alt="<?=IMG_ALT?>" />
+                            </div>
+                            <div class="row">
+                                <div class="col col-sm-6">
+                                    <img src="<?=ASSETS_URL?>frontend/images/tintuc-03.jpg" alt="<?=IMG_ALT?>" />
+                                </div>
+                                <div class="col col-sm-6">
+                                    <img src="<?=ASSETS_URL?>frontend/images/tintuc-04.jpg" alt="<?=IMG_ALT?>" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
